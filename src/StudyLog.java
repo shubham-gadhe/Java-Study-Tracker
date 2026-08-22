eb83dd0a4250be5f8591fee0565e7f2abf973eea
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-08-22 20:09:27 +0530

    Add files via upload

---
+++ src/StudyLog.java
@@ -0,0 +1,43 @@
+import java.time.LocalDate;
+
+class StudyLog
+{
+    private LocalDate Date;
+    private String Subject;
+    private double Duration;
+    private String Description;
+
+    public StudyLog(LocalDate a, String b, double c, String d)
+    {
+        this.Date = a;
+        this.Subject = b;
+        this.Duration = c;
+        this.Description = d;
+    }
+
+    @Override
+    public String toString()
+    {
+        return Date + " | " + Subject + " | " + Duration + " Hours | " + Description;
+    }
+
+    public LocalDate getDate()
+    {
+        return this.Date;
+    }
+
+    public String getSubject()
+    {
+        return this.Subject;
+    }
+
+    public double getDuration()
+    {
+        return this.Duration;
+    }
+
+    public String getDescription()
+    {
+        return this.Description;
+    }
+}
